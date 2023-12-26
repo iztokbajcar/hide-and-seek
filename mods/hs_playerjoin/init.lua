545f522b69f7f43632fb4e84d24f653df30abd7f
Iztok Bajcar <ib8548@student.uni-lj.si>
2023-12-26 18:13:10 +0100

    Made hiders appear as the block they are disguised with

--- mods/hs_playerjoin/init.lua
+++ mods/hs_playerjoin/init.lua
@@ -1,18 +1,38 @@
 hiders = {}
 seekers = {}
 
-function hide_nametag(player)
+transparent = {"transparent.png", "transparent.png", "transparent.png", "transparent.png", "transparent.png", "transparent.png"}
+
+function hide_player(player)
+    -- hide the player's nametag
     local c = player:get_nametag_attributes().color
     c.a = 0  -- set alpha to 0 to make it completely transparent
     player:set_nametag_attributes({color = c})
+
+    -- make the player transparent
+    default.player_set_textures(player, transparent)
+
+    -- make the player invisible on the minimap
+    player:set_properties({
+        show_on_minimap = false
+    })
 end
 
 function add_to_hiders(player)
     table.insert(hiders, player)
 
-    -- hide the player's nametag
-    -- (to prevent it from giving away the hider's location)
-    hide_nametag(player)
+    hide_player(player)
+
+    -- make the player shorter (to fit inside 1x1x1 openings)
+    player:set_properties({
+        collisionbox = {-0.5, 0.5, 0, 1, -0.5, 0.5},
+        eye_height = 0.25,
+    })
+
+    -- attach the player's disguise to them
+    local player_pos = player:get_pos()
+    local node = minetest.add_entity(player_pos, "hs_playerjoin:testentity")
+    node:set_attach(player, "", {x=0, y=0, z=0})  -- half a node above the ground
 
     minetest.log(player:get_player_name() .. " is now a hider")
 end
@@ -43,4 +63,26 @@ function player_join(player)
     end
 end
 
+-- minetest.register_on_mods_loaded(register_hider_model)
 minetest.register_on_joinplayer(player_join)
+
+-- register the node entity
+minetest.register_entity("hs_playerjoin:testentity", {
+    initial_properties = {
+        physical = true,
+        collide_with_objects = false,
+        collisionbox = {-0.5, -0.5, -0.5, 0.5, 0.5, 0.5},
+        selectionbox = {-0.5, -0.5, -0.5, 0.5, 0.5, 0.5},
+        visual = "cube",
+        shaded = true,
+        show_on_minimap = false,
+        textures = {
+            "farming_straw.png",
+            "farming_straw.png",
+            "farming_straw.png",
+            "farming_straw.png",
+            "farming_straw.png",
+            "farming_straw.png",
+        },
+    },
+});
