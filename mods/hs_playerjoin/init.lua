93caffb3a809190c230a281ef139ba8f0d095b84
Iztok Bajcar <ib8548@student.uni-lj.si>
2023-12-26 14:20:12 +0100

    Fixed team joining

--- mods/hs_playerjoin/init.lua
+++ mods/hs_playerjoin/init.lua
@@ -13,20 +13,24 @@ function add_to_hiders(player)
     -- hide the player's nametag
     -- (to prevent it from giving away the hider's location)
     hide_nametag(player)
+
+    minetest.log(player:get_player_name() .. " is now a hider")
 end
 
 function add_to_seekers(player)
     table.insert(seekers, player)
+    minetest.log(player:get_player_name() .. " is now a seeker")
 end
 
 function player_join(player)
     -- if the teams have a different number of players,
     -- assign the new player into the team with less players
     -- and pick a random team otherwise
+    minetest.log(#hiders .. " hiders, " .. #seekers .. " seekers")
     if #hiders > #seekers then
-        add_to_hiders(player)
-    elseif #seekers > #hiders then
         add_to_seekers(player)
+    elseif #seekers > #hiders then
+        add_to_hiders(player)
     else
         math.randomseed(os.clock())
         local r = math.random(0, 1)
