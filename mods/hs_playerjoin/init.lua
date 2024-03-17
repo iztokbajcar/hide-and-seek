a7c9e2ccd85699294c9053f2e265f86261bc4fce
Iztok Bajcar <ib8548@student.uni-lj.si>
2024-03-17 14:15:52 +0100

    Made hiders' entities assume the disguise of the last node they have been hiding as

--- mods/hs_playerjoin/init.lua
+++ mods/hs_playerjoin/init.lua
@@ -1,11 +1,14 @@
 player_team = {}  -- maps player names to team names
 hider_hiding = {}  -- maps player names to whether they are hiding (stationary) or not
-hider_node_name = {}  -- which node is a hider using as their disguise
+hider_node_name = {}  -- which node is a hider using as their disguise (player name --> node name)
 hider_node_pos = {}  -- where is the node that the hider is using as their disguise
 hider_entity = {} -- the entity that is attached to the hider
+disguise_entities = {}  -- stores all defined disguise entities (entity name --> entity table)
 num_hiders = 0
 num_seekers = 0
 
+disguise_entity_prefix = "hs_playerjoin:disguise_entity_"
+default_disguise_node = "default:brick"
 
 transparent = {"transparent.png", "transparent.png", "transparent.png", "transparent.png", "transparent.png", "transparent.png"}
 
@@ -43,8 +46,9 @@ end
 -- makes them face towards the positive z-axis,
 -- makes their disguise invisible
 -- and spawns a new node at their location
-function put_hider_into_hiding(player)
+function put_hider_into_hiding(player, node_name)
     local player_name = player:get_player_name()
+
     if player_team[player_name] ~= "hider" then
         return
     end
@@ -68,13 +72,19 @@ function put_hider_into_hiding(player)
 
     -- hide the player and their block entity
     local entity = hider_entity[player_name]
-    entity:set_properties({is_visible = false})
-    player:set_properties({pointable = false})
+    
+    if entity ~= nil then
+        entity:set_properties({is_visible = false})
+        player:set_properties({pointable = false})
+        minetest.log("Hid the disguise entity for player " .. player_name)
+    else
+        minetest.warn("Could not find the active disguise entity for player " .. player_name)
+    end
 
     -- place a node at the player's position
-    local node_name = hider_node_name[player_name]
+    -- local node_name = hider_node_name[player_name]
     if node_name == nil then
-        node_name = "farming:straw"
+        node_name = node_name
     end
     minetest.set_node(new_pos, {name=node_name})
     hider_node_pos[player_name] = new_pos
@@ -95,10 +105,23 @@ function put_hider_out_of_hiding(player)
         minetest.remove_node(hider_node_pos[player_name])
     end
 
+    -- change the player's entity to the new one
+    if hider_entity[player_name] ~= nil then
+        hider_entity[player_name]:remove()
+        hider_entity[player_name] = nil
+    end
+
     -- make the player and their block entity visible again
-    local entity = hider_entity[player_name]
-    entity:set_properties({is_visible = true})
-    player:set_properties({pointable = true})
+    -- local entity = hider_entity[player_name]
+    local node_name = hider_node_name[player_name]
+    local entity_name = disguise_entity_prefix .. node_name:gsub(":", "_")
+    local entity = minetest.add_entity(player:get_pos(), entity_name)
+
+    -- define the on_punch callback for the entity
+    attach_punch_callback_to_disguise_entity(entity)
+
+    hider_entity[player_name] = entity
+    attach_disguise_entity_to_player(entity, player)
 
 
     minetest.log(player_name .. " is no longer in hiding")
@@ -119,8 +142,9 @@ end
 
 function damage_hider(hider, puncher, damage)
     local hider_hp = hider:get_hp()
+
     if hider_hp > 0 then
-        hider:set_hp(hider_hp - damage, {type = "punch", object = puncher})
+        hider:set_hp(hider_hp - damage, {type = "punch", object = puncher.object})
         minetest.log("Hider " .. hider:get_player_name() .. " damaged by " .. puncher:get_player_name())
     end
 end
@@ -135,6 +159,19 @@ function on_hider_death(hider)
     minetest.log("Hider " .. hider:get_player_name() .. " died")
     -- remove the hider's entity
     remove_hider_entity(hider)
+end
+
+function attach_disguise_entity_to_player(entity, player)
+    local player_name = player:get_player_name()
+    entity:get_luaentity()._player_name = player_name
+    entity:set_attach(player, "", {x=0, y=0, z=0})
+    hider_entity[player_name] = entity
+end
+
+function attach_punch_callback_to_disguise_entity(entity)
+    entity:get_luaentity().on_punch = function(self, puncher, time_from_last_punch, tool_capabilities, dir, damage)
+        on_disguise_entity_punch(self, puncher, time_from_last_punch, tool_capabilities, dir, damage)
+    end
 end
 
 function add_to_hiders(player)
@@ -152,12 +189,14 @@ function add_to_hiders(player)
     })
 
     -- attach the player's disguise to them
+    -- we use a hardcoded default entity for the hider which
+    -- they get when they join and retain it before they punch a node
+    -- but we could instead choose a random block 
     local player_pos = player:get_pos()
-    local entity = minetest.add_entity(player_pos, "hs_playerjoin:disguise_entity")
-    hider_node_name[player_name] = "farming:straw"  -- TODO change later
-    entity:get_luaentity()._player_name = player_name
-    entity:set_attach(player, "", {x=0, y=0, z=0})
-    hider_entity[player_name] = entity
+    local entity = minetest.add_entity(player_pos, disguise_entity_prefix .. default_disguise_node:gsub(":", "_"))
+    attach_punch_callback_to_disguise_entity(entity)
+    hider_node_name[player_name] = default_disguise_node
+    attach_disguise_entity_to_player(entity, player)
 
     minetest.log(player_name .. " is now a hider")
 end
@@ -206,58 +245,138 @@ end
 
 -- when a player respawns, send them to the spawn area
 function player_respawn(player)
-    pos = hs_maps.spawn_pos
+    local pos = hs_maps.spawn_pos
     pos.x = pos.x + 100
     pos.y = pos.y + 1
     pos.z = pos.z + 100
     player:set_pos(pos)
+
+    if player_team[player:get_player_name()] == "hider" then
+        put_hider_out_of_hiding(player)
+    end
+
     return true
 end
 
 -- minetest.register_on_mods_loaded(register_hider_model)
 minetest.register_on_joinplayer(player_join)
 
--- register the node entity
-local disguise_entity = {
-    initial_properties = {
-        hp_max = 50,
-        physical = true,
-        collide_with_objects = false,
-        collisionbox = {-0.5, -0.5, -0.5, 0.5, 0.5, 0.5},
-        selectionbox = {-0.5, -0.5, -0.5, 0.5, 0.5, 0.5},
-        visual = "cube",
-        shaded = true,
-        show_on_minimap = false,
-        textures = {
-            "farming_straw.png",
-            "farming_straw.png",
-            "farming_straw.png",
-            "farming_straw.png",
-            "farming_straw.png",
-            "farming_straw.png",
+-- registers disguise entities for all nodes in the default mod
+function register_disguise_entities_for_nodes_in_default_mod()
+    for _, node in pairs(minetest.registered_nodes) do
+        if node.drawtype == "normal" then
+            register_disguise_entity(node.name)
+        end
+    end
+end
+
+-- registers a disguise entity for the given node
+function register_disguise_entity(node_name)
+    -- check if the node is registered
+    if minetest.registered_nodes[node_name] == nil then
+        return
+    end
+    
+    local node = minetest.registered_nodes[node_name]
+    local entity_name = disguise_entity_prefix .. node_name:gsub(":", "_")
+
+    -- check if the disguise entity has already been registered
+    -- for this node
+    if minetest.registered_entities[entity_name] ~= nil then
+        return
+    end
+
+    -- generate the texture table
+    local entity_textures = {}
+    if #node.tiles == 1 then
+        local t = node.tiles[1]
+        entity_textures = {t, t, t, t, t, t}
+    elseif #node.tiles == 2 then
+        local t1 = node.tiles[1]
+        local t2 = node.tiles[2]
+        entity_textures = {t1, t1, t2, t2, t2, t2}
+    else
+        for _, t in pairs(node.tiles) do
+            table.insert(entity_textures, t)
+        end
+        while #entity_textures < 6 do
+            table.insert(entity_textures, entity_textures[#entity_textures])
+        end
+    end
+
+    for i = 1, 6 do
+        -- an element of the tiles table can also be another table
+        -- which contains the texture name, but also defines some additional
+        -- properties; we need to extract the texture name in that case
+        if type(entity_textures[i]) == "table" then
+            if  entity_textures[i].name ~= nil then
+                entity_textures[i] = entity_textures[i].name
+            elseif entity_textures[i].image ~= nil then
+                entity_textures[i] = entity_textures[i].image
+            end
+        end
+    end
+
+    -- create the entity
+    local disguise_entity = {
+        initial_properties = {
+            hp_max = 50,
+            physical = true,
+            collide_with_objects = false,
+            collisionbox = {-0.5, -0.5, -0.5, 0.5, 0.5, 0.5},
+            selectionbox = {-0.5, -0.5, -0.5, 0.5, 0.5, 0.5},
+            visual = "cube",
+            shaded = true,
+            show_on_minimap = false,
+            textures = entity_textures,
         },
-    },
-    _player_name = nil
-}
+        _player_name = nil
+    }
 
-function disguise_entity:on_punch(puncher, time_from_last_punch, tool_capabilities, dir, damage)
+    
+
+    -- register the entity
+    minetest.register_entity(entity_name, disguise_entity)
+    disguise_entities[entity_name] = disguise_entity
+    minetest.log("Registered a disguise entity for node " .. node_name)
+end
+
+function on_disguise_entity_punch(self, puncher, time_from_last_punch, tool_capabilities, dir, damage)
     local hider_name = self._player_name
-
+    
     if hider_name == nil then
         return
     end
 
     local hider = minetest.get_player_by_name(hider_name)
-    minetest.log("Hider " .. hider_name .. " was punched")
+    minetest.log("Hider " .. hider_name .. " was punched by " .. puncher:get_player_name())
     damage_hider(hider, puncher, damage)
 end
 
-minetest.register_entity("hs_playerjoin:disguise_entity", disguise_entity)
-
 minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
-    if player_team[puncher:get_player_name()] == "hider" then
-        -- if the puncher is a hider, put them in hiding
-        put_hider_into_hiding(puncher)
+    local puncher_name = puncher:get_player_name()
+    if player_team[puncher_name] == "hider" then
+        local textures = minetest.registered_nodes[node["name"]].tiles
+
+        -- if the puncher is a hider and they are not already hiding,
+        -- put them in hiding
+        if not hider_hiding[puncher_name] then
+            -- get the name of the punched block
+            local node_name = node["name"]
+
+            local entity_name = disguise_entity_prefix .. node_name:gsub(":", "_")
+
+            -- if the disguise entity for this node exists,
+            -- put the hider in hiding
+            if disguise_entities[entity_name] ~= nil then
+                -- set the hider's node to the punched node
+                hider_node_name[puncher_name] = node_name
+
+                put_hider_into_hiding(puncher, hider_node_name[puncher_name])
+            else
+                -- TODO notify the player that they cannot hide as this node
+            end
+        end
     elseif player_team[puncher:get_player_name()] == "seeker" then
         -- if the puncher is a seeker, check if they punched a node hiding a hider
         -- if so, unhide the punched hider and damage them
@@ -281,3 +400,7 @@ minetest.register_on_dieplayer(function(player, reason)
 end)
 
 minetest.register_on_respawnplayer(player_respawn)
+
+-- register disguise entities for all nodes in the default mod
+register_disguise_entities_for_nodes_in_default_mod()
+
